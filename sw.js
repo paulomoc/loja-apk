2e81e049803835d56194fe4f139d0ac87f2a9e16
paulomoc <90142719+paulomoc@users.noreply.github.com>
2025-12-25 17:39:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,12 +13,33 @@ const ASSETS = [
 
 // Install Service Worker
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Take over immediately
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             // We use addAll but in a local file context it might fail for some assets, 
             // so we handle it gracefully.
             return cache.addAll(ASSETS).catch(err => console.log('Cache error:', err));
         })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        Promise.all([
+            // Take control of all clients immediately
+            clients.claim(),
+            // Clean up old caches
+            caches.keys().then((cacheNames) => {
+                return Promise.all(
+                    cacheNames.map((cacheName) => {
+                        if (cacheName !== CACHE_NAME) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        ])
     );
 });
 
